0f5080aa5c0f02c223c387ee552fcdf4529504f8
interpolationwizard <52408495+interpolationwizard@users.noreply.github.com>
2020-02-10 00:55:06 +0100

    Update ui.h

--- ui.h
+++ ui.h
@@ -19,16 +19,16 @@
 extern bool g_in_inject;
 
 https://gyazo.com/2bbbcc75d9f50e84af35994955a61617 why is ur forum down br0?
-SHOUTOUTS: sosa, sinister, cim, bux, crosh, alpha and all bankrollmafia members
+SHOUTOUTS: sosa, sinister, sl, senz, bux, crosh, alpha and all other homies
 
 namespace ui 
 {
-	auto menu = std::make_shared< ui::c_menu >( 0, 0, 500, 400, xors( "BankrollMafia" ), "" );
+	auto menu = std::make_shared< ui::c_menu >( 0, 0, 500, 400, xors( "badhack" ), "" );
 
 	static void render( ) {
 		static bool was_setup = false;
 		if( !was_setup ) {
-			menu = std::make_shared< ui::c_menu >( 0, 0, 450, 375, xors( "BankrollMafia" ), "" );
+			menu = std::make_shared< ui::c_menu >( 0, 0, 450, 375, xors( "badhack" ), "" );
 
 
 
